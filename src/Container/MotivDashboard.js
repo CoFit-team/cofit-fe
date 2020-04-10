7972e6b4b42158e12a461f60018f4e58f2eddb23
Valerie Lui <vallq@users.noreply.github.com>
2020-04-10 22:56:21 +0800

    add Dashboard with testing chart functionality

--- src/Container/MotivDashboard.js
+++ src/Container/MotivDashboard.js
@@ -1,1 +1,34 @@
-import react from "react"
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+class MotivDashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = { dashBoardState: "" };
+    this.data = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "my happy emotions",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, 10, 5, 2, 20, 30, 45],
+        },
+      ],
+    };
+  }
+
+  createChart = (data) => {
+    return (
+      <div>
+        <Bar data={data} />
+      </div>
+    );
+  };
+
+  render() {
+    return <div>{this.createChart(this.data)}</div>;
+  }
+}
+
+export default MotivDashboard;
