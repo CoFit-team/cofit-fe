7fca4a3726b997cfc0f78219881c95f01b9e3db5
Valerie Lui <vallq@users.noreply.github.com>
2020-04-10 22:54:41 +0800

    add route to dashboard

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MotivGallery from "./Container/MotivGallery";
+import MotivDashboard from "./Container/MotivDashboard";
+import Navbar from "./Components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar />
         <Switch>
-          <Route path="/" component={MotivGallery} />
-          <h1>Hello World</h1>
+          <Route exact path="/" component={MotivGallery} />
+          <Route exact path="/dashboard" component={MotivDashboard} />
         </Switch>
       </div>
     </BrowserRouter>
